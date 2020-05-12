3e02aef245f73d98a288cc382a47555a8f289972
Sean King <sean@seanking.co>
2020-05-12 19:06:50 +1200

    Add asset file declarations

---
+++ template/types/assets.d.ts
@@ -0,0 +1,15 @@
+// Images
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.webm';
+
+// Fonts
+declare module '*.woff2';
+declare module '*.woff';
+
+// Documents
+declare module '*.txt';
+declare module '*.pdf';

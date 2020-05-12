af7de56da96cb4a36637e154a1e9c327cf5a2364
Sean King <sean@seanking.co>
2020-05-12 19:40:59 +1200

    make modal demo a little nicer

--- template/src/components/Modal/Modal.stories.tsx
+++ template/src/components/Modal/Modal.stories.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Modal, ModalProps } from '.';
 import { css } from '@emotion/core';
+import { Heading } from '../Heading';
+import { Copy } from '../Copy';
 
 export default {
   title: 'Components/Modal',
@@ -22,11 +24,10 @@ export const Default = () => {
         open={isOpen}
         onClose={() => setIsOpen(false)}
       >
-        <h1>Modal</h1>
-        <p>
-          Egestas porta donec suscipit tempor aenean ultricies senectus
-          fringilla taciti
-        </p>
+        <Heading>Modal</Heading>
+        <Copy
+          content="<p>Egestas porta donec suscipit tempor aenean ultricies senectusfringilla taciti</p>"
+        />
       </Modal>
     </>
   );

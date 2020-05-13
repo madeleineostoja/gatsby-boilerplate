dfdbfcee707738fa6063a9742cdfa34956c1860c
Sean King <sean@seanking.co>
2020-05-13 13:46:37 +1200

    Update assets declarations

--- template/types/assets.d.ts
+++ template/types/assets.d.ts
@@ -1,14 +1,23 @@
+// Code
+declare module '*.css';
+
 // Images
 declare module '*.svg';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.gif';
+declare module '*.webp';
+
+// Media
+declare module '*.mp4';
 declare module '*.webm';
 
 // Fonts
 declare module '*.woff2';
 declare module '*.woff';
+declare module '*.ttf';
+declare module '*.eot';
 
 // Documents
 declare module '*.txt';

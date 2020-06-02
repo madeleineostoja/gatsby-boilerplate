a91970d26fe984c74fc8ce0d57e20ff34d1b6647
Sean King <sean@seanking.co>
2020-06-02 17:06:32 +1200

    Add CSS setProperties helper

--- template/src/lib/css.ts
+++ template/src/lib/css.ts
@@ -2,6 +2,16 @@ import { css } from '@emotion/core';
 import { subgrid } from 'satchel-css';
 
 /**
+ * Helper to update CSS variables
+ * @param properties Object of CSS properties
+ */
+export function setProperties(properties: any) {
+  Object.keys(properties).forEach((property: string) =>
+    document.documentElement.style.setProperty(property, properties[property])
+  );
+}
+
+/**
  * Global breakpoints
  */
 export const BREAKPOINTS = {
